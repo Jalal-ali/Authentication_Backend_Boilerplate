cfa962221f7127f835668c1a00b100a6cc3c0743
Jalal Ali <alikhanumar795@gmail.com>
2026-05-04 18:07:09 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import authRoutes from "./src/routes/auth.route.js"
 import connectDB from "./src/db/index.js";
+import dotenv from 'dotenv';
+dotenv.config();
 
+const port = process.env.PORT ;
 const app = express();
 app.use(express.json())
 app.use(cors())
@@ -11,10 +14,9 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
 connectDB();
 app.use("/api/v1", authRoutes );
-app.listen(3000, ( req,res) => {
-  console.log("Server running on port 3000");
+app.listen(port, (req, res) => {
+  console.log(`Server running on port ${port}`);
 
 });
